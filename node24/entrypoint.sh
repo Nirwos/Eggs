6d548174dd98028486cfb6882dad7eefbf53501b
Nirwos <139747296+Nirwos@users.noreply.github.com>
2025-09-21 20:56:03 +0000

    xD

--- node24/entrypoint.sh
+++ node24/entrypoint.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-# Załaduj nvm (jeśli potrzebujesz)
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+cd /home/container
 
-# Uruchom aplikację Node.js
-exec "$@"
+# Make internal Docker IP address available to processes.
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# Print Node.js Version
+node -v
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
